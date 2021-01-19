ae813f7519dac986d1676f1c51814b2c54a8f73b
MrNaughtZero <neil.naught@icloud.com>
2021-01-19 20:35:01 +0000

    test! ;

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
-app.secret_key = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+print(os.environ.get('SECRET_KEY'))
 
 from .routes import main,admin,auth
  
